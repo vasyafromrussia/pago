64607babd8a7fbcb6ee6c9083f9f845f0ff2cfaa
Vasily Styagov <vasily.shc@gmail.com>
2016-07-20 18:25:08 +0300

    javadoc

--- library/src/main/java/io/octo/bear/pago/Pago.java
+++ library/src/main/java/io/octo/bear/pago/Pago.java
@@ -13,10 +13,9 @@ import rx.Single;
 
 /**
  * Created by shc on 14.07.16.
- *
  * <p>
- *     This class is entry point to the wonderful world of Play Store in-app purchases. <br/>
- *     There's couple of methods for each purchase-related action (for products and subscriptions).
+ * This class is entry point to the wonderful world of Play Store in-app purchases. <br/>
+ * There's couple of methods for each purchase-related action (for products and subscriptions).
  * </p>
  */
 public class Pago {
@@ -70,22 +69,55 @@ public class Pago {
         return new ProductDetailsSingle(context, PurchaseType.SUBSCRIPTION, skus);
     }
 
+    /**
+     * Here you can start to spend your money. This method starts <i>product</i> purchase flow and performs purchase.
+     *
+     * @param sku     product id of desired product
+     * @param payload some arbitrary data, that purchased item info will contain
+     * @return {@link Single} emits {@link Order} containing purchased item info and signature
+     */
     public Single<Order> purchaseProduct(final String sku, final String payload) {
         return new PerformPurchaseSingle(context, PurchaseType.INAPP, sku, payload);
     }
 
+    /**
+     * Here you can start to spend your money. This method starts <i>subscription</i> purchase flow and performs purchase.
+     *
+     * @param sku     product id of desired product
+     * @param payload some arbitrary data, that purchased item info will contain
+     * @return {@link Single} that emits {@link Order} containing purchased item info and signature
+     */
     public Single<Order> purchaseSubscription(final String sku, final String payload) {
         return new PerformPurchaseSingle(context, PurchaseType.SUBSCRIPTION, sku, payload);
     }
 
+    /**
+     * Use this method to check <i>products</i>, that user have already bought.
+     *
+     * @return {@link Single} that emits {@link PurchasedItem} containing purchased products data
+     */
     public Single<List<PurchasedItem>> obtainPurchasedProducts() {
         return new PurchasedItemsSingle(context, PurchaseType.INAPP);
     }
 
+    /**
+     * Use this method to check <i>subscription</i>, that user have already bought.
+     *
+     * @return {@link Single} that emits {@link PurchasedItem} containing purchased subscriptions data
+     */
     public Single<List<PurchasedItem>> obtainPurchasedSubscriptions() {
         return new PurchasedItemsSingle(context, PurchaseType.SUBSCRIPTION);
     }
 
+    /**
+     * If user already owns some product, Google Play would prevent user from purchasing another product with the same
+     * product id. You have to consume this purchase and make the product available again.
+     * And yes, you can do it with this method. <br/>
+     * <b>Note:</b> subscriptions cannot be consumed.
+     *
+     * @param purchaseToken token of purchased product, can be taken from {@link Order} or {@link PurchasedItem}
+     * @return {@link Completable} that notifies you about either successful consumption, or error
+     */
     public Completable consumeProduct(final String purchaseToken) {
         return new ConsumePurchaseCompletable(context, purchaseToken);
     }
